c1ecc1cf9318ec189bade15d956ef1f8ec011caf
Cassian0 <schmitz.cassiano@gmail.com>
2020-11-19 22:15:34 -0300

    ajustes na API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+const cors = require ('cors');
 const express = require('express');
 
 const routes = require('./routes');
 const logRequests = require('./middlewares');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(logRequests);
 app.use(routes);
 
-app.listen(3000, () => {
+/*Ouvindo portas "PORT" no caso do Heroku para usarmos o servidor Heroku*/
+//3000 para rodarmos localmente
+app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor iniciado com sucesso!');
 });
